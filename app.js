d6ecef8b363c151fd67fae3dc01c84eea4c28f07
Austin Wood <i@austinwood.me>
2014-12-09 19:52:49 -0700

    First working equalizer via socket

--- app.js
+++ app.js
@@ -8,24 +8,29 @@ var password = 'ReadUserPassword'; // TODO
 var url = '104.236.60.203'; // TODO
 var db = mongoskin.db('mongodb://'+username+':'+password+'@'+url+':27018/sound', {safe:true})
 
-
-
-
-
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
 app.get('/',function(req,res){
 
-    var query;
-    db.collection('noise').findOne(function(err, result) {
-          if (err) throw err; 
-          
-          console.log(result.noise.avg60s);
-          res.send(result);
-      }); 
- });
+  var query;
+  db.collection('noise').findOne(function(err, result) {
+    if (err) throw err;
+    console.log(result.noise.avg60s);
+    res.send(result);
+  });
+});
+
+app.get('/',function(req,res){
+
+  var query;
+  db.collection('noise').findOne(function(err, result) {
+    if (err) throw err;
+    console.log(result.noise.avg60s);
+    res.send(result);
+  });
+});
 
 //this was supposed to be the API, depricated as of now. 
 app.get('/measures',function(req,res){
@@ -38,28 +43,34 @@ app.get('/measures',function(req,res){
   if (!query.hasOwnProperty('finish')){ 
     res.send('Bad finish parameter, use format: /measures?start=55&finish=55')
   }
-
-  else { 
+  else {
     start = parseInt(query.start);
     finish =  parseInt(query.finish);
     res.send('Measures ' + start + ' ' + finish);
-
     //make mongo query here using start and finish params 
-
     //this is where we put the datafile to be sent.  
     //res.sendfile('sample_data.csv')
-  } 
+  }
 });
 
-
-//path to austin's visualization
-app.get('/visual',function(req,res){
-  res.render('visual/visual.html');
+//path to Austin's equalizer
+app.get('/equalizer', function(req,res){
+  res.render('equalizer/equalizer.html');
 });
+
+// //path to Austin's equalizer data
+// app.get('/equalizerdata', function(req, res){
+//   data = db.collection('noise').find({'frequency.type': 'logbin18'}, {'frequency.values':1, _id:0}).limit(1).toArray(function(err, result){
+//     if (err) throw err;
+//     res.json(result);
+//   })
+// });
+
 //path for jake's visualization to render the html file
 app.get('/livevisual',function(req,res){
   res.render('livevisual/livevisual.html');
 });
+
 //path for jake's visualization to render the html file
 app.get('/livevisualdata',function(req,res){
   data = db.collection('noise').find({location:'microphone'}).sort({"date":-1}).limit(1).toArray(function(err,result){
@@ -67,36 +78,7 @@ app.get('/livevisualdata',function(req,res){
    res.json(result);
   })
 });
-//path to the calendar
-app.get('/equalizer',function(req,res){
-  //we want our mongo query to find data a year back, break it down by day, and find the avg of the averages over that day 
-  
-  //var end = new Date();
-  //start date is year plus 1900; the same month ; 7 days before which rolls back properly 
-  //var start = end;
-  //start.setDate(end.getDate()-7)
- console.log('++++++++++++++++++++++++++++++++++') 
-  
-  db.collection('noise').find().sort({datefield:-1}).limit(10).toArray(function(err,result){
-   if (err) throw err;
-   console.log(result);
-  })
-  
-  //var query = {'date':{$gte:start,$lt:end}};
-  var projection = {};
-
-  /*db.collection('noise').findOne(function(err, result) {
-        if (err) throw err; 
-        
-        console.log(result.noise.avg60s);
-       
-  });*/ 
-  res.render('equalizer/equalizer.html');
-});
 
 app.listen(13000);
 console.log('listening on port 13000');
-
-
-
 
