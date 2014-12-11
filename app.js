3617d7244525f7fd1109f992d42bc1533a377a36
Niklas Fejes <niklas.fejes@colorado.edu>
2014-12-11 01:19:32 -0700

    stuff

--- app.js
+++ app.js
@@ -8,13 +8,30 @@ var password = 'ReadUserPassword'; // TODO
 var url = '104.236.60.203'; // TODO
 var db = mongoskin.db('mongodb://'+username+':'+password+'@'+url+':27018/sound', {safe:true})
 
+var toObjectID = mongoskin.helper.toObjectID;
+
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
 // combined visualizations
 app.get('/',function(req,res){
-  res.render('index.html');
+  res.render('index.html',{data:[]});
+});
+app.get('/new',function(req,res){
+	var d = new Date(Date.now() - 15*60*60*1000);
+	db.collection('noise').aggregate([
+		{ $match : { location : 'microphone', date : { $gt : d } } },
+		{ $sort: { date : 1 }},
+		{ $limit: 30},
+	], function (err,mongoRes) {
+		if (err) console.log(err);
+  		res.render('index.html',{
+			data: (mongoRes || []).map(function(d) {
+				return { l: d.noise.level, t: d.date }
+			})
+		});
+	});
 });
 
 // path to Austin's equalizer
@@ -34,6 +51,41 @@ app.get('/livevisualdata',function(req,res){
    res.json(result);
   })
 });
+
+// Dawson's mongo data collection
+app.get('/livevisualdata2',function(req,res) {
+	if ('firstDate' in req.query) {
+		var d = new Date(req.query.firstDate);
+		if (isNaN(d.getTime())) {
+			res.json(['invalid date']);
+			return;
+		}
+		db.collection('noise').aggregate([
+			{ $match : { location : 'microphone', date : { $gt : d } } },
+			{ $sort: { date : 1 }},
+			{ $limit: 30},
+		], function (err,mongoRes) {
+			if (err) console.log(err);
+			res.json(mongoRes || []);
+		});
+	}
+	if ('lastId' in req.query) {
+		var oid = toObjectID(req.query.lastId);
+		//console.log('id: ' + req.query.lastId + ' : ' + oid);
+		db.collection('noise').aggregate([
+			{ $match : { location : 'microphone', _id : { $gt : oid } } },
+			{ $sort: { date : 1 }},
+			{ $limit: 30},
+		], function (err,mongoRes) {
+			if (err) console.log(err);
+			res.json(mongoRes || []);
+		});
+	}
+	else {
+		res.json(['error']);
+	}
+});
+
 
 app.listen(13000);
 console.log('listening on port 13000');
